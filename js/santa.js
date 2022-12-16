e469083d07d68dcec0c39660522e698c356acc76
artalcb <artalcb@gmail.com>
2022-12-16 14:13:40 +0100

    second steps

--- js/santa.js
+++ js/santa.js
@@ -5,15 +5,16 @@ var svg = document.getElementById('svg');
 var cursorX;
 var cursorY;
 document.onmousemove = function (e) {
-    cursorX = e.pageX;
-    cursorY = e.pageY;
-    posY = Math.abs(1000-cursorY);
+    cursorX = (e.pageX / window.innerWidth);
+    cursorY = (e.pageY / window.innerHeight)*100;
+    posY = (Math.abs(cursorY-100));
+    
+    let shotWidthPx = window.getComputedStyle(shotgun).getPropertyValue("width");
+    
+    let shotWidth = (parseInt(shotWidthPx.replace(/px/,"")));
 
-    //let ver = window.getComputedStyle(svg).getPropertyValue("left")
-    //let newPosX = (cursorX-(parseInt(ver.replace(/px/,""))));
-
-    svg.style.transform =  "translateX(-10px) scaleY(" + posY/8 + "%)";
-    shotgun.style.transform = "translateX("+cursorX+"px)";
-
+    svg.style.transform =  "translateX(-10px) scaleY(" + posY + "%)";
+    shotgun.style.transform = "translateX("+window.innerWidth/2+"px) rotate("+(cursorX-0.5)*50+"deg)";
+    
 }
 
